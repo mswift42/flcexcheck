fd8b3c2278acbc9178990d9a117f1fc71a854c42
mswift42 <martin.haesler@gmail.com>
2018-09-17 14:49:01 +0100

    add field stores.

--- lib/widgets/searchwidget.dart
+++ lib/widgets/searchwidget.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flcexcheck/store.dart' show Store;
 
 class SearchWidget extends StatefulWidget {
-
   _SearchWidgetState createState() => _SearchWidgetState();
 }
 
 class _SearchWidgetState extends State<SearchWidget> {
   Store activeStore;
   String searchquery;
+  var _stores = [
+    Store("Rose Street", "54"),
+    Store("Cameron Toll", "3015"),
+    Store("Leith", "3117")
+  ];
   final controller = TextEditingController();
 
   void _searchProduct(String inp) {
@@ -16,18 +20,22 @@ class _SearchWidgetState extends State<SearchWidget> {
       searchquery = inp;
     });
   }
+
   @override
   Widget build(BuildContext context) {
-  return Container(
-    child: Column(
-      children: <Widget>[
-        TextField(
-          controller: controller,
-          onSubmitted: _searchProduct,
-        ),
-        Text(searchquery),
-      ],
-    ),
-  );
+    return Container(
+      child: Column(
+        children: <Widget>[
+          TextField(
+            controller: controller,
+            onSubmitted: _searchProduct,
+          ),
+          Text(searchquery),
+          Row(
+            children: <Widget>[Radio<int>()],
+          )
+        ],
+      ),
+    );
   }
 }
