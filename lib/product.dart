23a1844fd2124c280a813e37f7c6b1a62fef5137
mswift42 <martin.haesler@gmail.com>
2018-09-18 06:17:42 +0100

    add factory method fromJson.

--- lib/product.dart
+++ lib/product.dart
@@ -2,4 +2,12 @@ class Product {
    String title, thumbnail, price, url;
 
    Product(this.title, this.thumbnail, this.price, this.url);
+   factory Product.fromJson(Map<String, dynamic> json) {
+     return Product(
+       json['title'],
+       json['thumbnail'],
+       json['price'],
+       json['url'],
+     );
+   }
 }
