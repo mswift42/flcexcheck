604d0c0df053980cde4f2ca47cd14a426adb1e08
mswift42 <martin.haesler@gmail.com>
2018-09-19 06:27:06 +0100

    fix price type in mockproducts.

--- lib/mockproducts.dart
+++ lib/mockproducts.dart
@@ -1,39 +1,34 @@
 import 'package:flcexcheck/product.dart' show Product;
+
 var mockProducts = [
   Product(
-    "Sony Xperia Tablet Z2 16GB, Unlocked C",
-    "https://uk.webuy.com/product_images/Computing/Tablets%20Android/SSONSGP52116GUNLC_l.jpg",
-    170,
-    "https://uk.webuy.com/product-detail?id=SSONSGP52116GUNLC"
-),
+      "Sony Xperia Tablet Z2 16GB, Unlocked C",
+      "https://uk.webuy.com/product_images/Computing/Tablets%20Android/SSONSGP52116GUNLC_l.jpg",
+      170,
+      "https://uk.webuy.com/product-detail?id=SSONSGP52116GUNLC"),
   Product(
-    "Sony Xperia X F5121 32GB Graphite Black, Tesco B",
-    "https://uk.webuy.com/product_images/Phones/Phones%20Android/SSONF512132GGBTESB_l.jpg",
-    125,
-    "https://uk.webuy.com/product-detail?id=SSONF512132GGBTESB"
-  ),
+      "Sony Xperia X F5121 32GB Graphite Black, Tesco B",
+      "https://uk.webuy.com/product_images/Phones/Phones%20Android/SSONF512132GGBTESB_l.jpg",
+      125,
+      "https://uk.webuy.com/product-detail?id=SSONF512132GGBTESB"),
   Product(
-    "Sony Xperia M4 Aqua 8GB Black, Unlocked B",
-    "https://uk.webuy.com/product_images/Phones/Phones%20Android/SSONE23038GBUNLB_l.jpg",
-    115,
-    "https://uk.webuy.com/product-detail?id=SSONE23038GBUNLB"
-  ),
+      "Sony Xperia M4 Aqua 8GB Black, Unlocked B",
+      "https://uk.webuy.com/product_images/Phones/Phones%20Android/SSONE23038GBUNLB_l.jpg",
+      115,
+      "https://uk.webuy.com/product-detail?id=SSONE23038GBUNLB"),
   Product(
-    "Sony Xperia L1 Black, EE B",
-    "https://uk.webuy.com/product_images/Phones/Phones%20Android/SSONG3311BEVEB_l.jpg",
-    105,
-    "https://uk.webuy.com/product-detail?id=SSONG3311BEVEB"
-  ),
+      "Sony Xperia L1 Black, EE B",
+      "https://uk.webuy.com/product_images/Phones/Phones%20Android/SSONG3311BEVEB_l.jpg",
+      105,
+      "https://uk.webuy.com/product-detail?id=SSONG3311BEVEB"),
   Product(
-    "Sony Xperia M4 Aqua 8GB Black, Unlocked C",
-    "https://uk.webuy.com/product_images/Phones/Phones%20Android/SSONE23038GBUNLC_l.jpg",
-    100,
-    "https://uk.webuy.com/product-detail?id=SSONE23038GBUNLC"
-  ),
+      "Sony Xperia M4 Aqua 8GB Black, Unlocked C",
+      "https://uk.webuy.com/product_images/Phones/Phones%20Android/SSONE23038GBUNLC_l.jpg",
+      100,
+      "https://uk.webuy.com/product-detail?id=SSONE23038GBUNLC"),
   Product(
-    "Sony Xperia M4 Aqua 8GB Silver, EE B",
-    "https://uk.webuy.com/product_images/Phones/Phones%20Android/SSONE23038GSEVEB_l.jpg",
-    85,
-    "https://uk.webuy.com/product-detail?id=SSONE23038GSEVEB"
-  )
+      "Sony Xperia M4 Aqua 8GB Silver, EE B",
+      "https://uk.webuy.com/product_images/Phones/Phones%20Android/SSONE23038GSEVEB_l.jpg",
+      85,
+      "https://uk.webuy.com/product-detail?id=SSONE23038GSEVEB")
 ];
